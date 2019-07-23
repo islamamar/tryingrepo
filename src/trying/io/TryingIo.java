2d89c7d9443896d58fbfbaf95cda967db779e588
islam <161100>
2019-07-23 22:27:37 +0300

    this is trying project

---
+++ src/trying/io/TryingIo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trying.io;
+
+/**
+ *
+ * @author 123456789
+ */
+public class TryingIo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainMenu frame = new MainMenu(); 
+        frame.setVisible(true);
+        
+    }
+    
+}
